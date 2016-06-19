6aa2f50bb50e980e82246fd7f7c21257ccb81f41
John Otander <johnotander@gmail.com>
2016-06-18 18:23:41 -0600

    Handle errors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fetch = require('isomorphic-fetch')
+const isPresent = require('is-present')
 
 module.exports = function coinr (currency) {
-
   return new Promise(function (resolve, reject) {
-    fetch(`https://api.coinmarketcap.com/v1/ticker/${currency}`).then(d => {
-      resolve(d.json().then(d => d[0]))
-    })
+    fetch(`https://api.coinmarketcap.com/v1/ticker/${currency}`)
+      .then(d => {
+        d.json().then(d => {
+          const response = d[0]
+          isPresent(response) ? resolve(response) : reject(response)
+        })
+      })
+      .catch(e => {
+        reject(e)
+      })
   })
 }
--- test.js
+++ test.js
@@ -18,3 +18,19 @@ test.cb('grabs ethereum data', t => {
       t.end()
     })
 })
+
+test.cb('rejects with unknown currency', t => {
+  t.plan(1)
+
+  coinr('foobar')
+    .then(d => {
+      console.log(d)
+      t.true(false)
+      t.end()
+    })
+    .catch(e => {
+      console.log(e)
+      t.true(true)
+      t.end()
+    })
+})
