29bbf78cee065b548be9c036086d43c3d862b4b0
John Otander <johnotander@gmail.com>
2016-07-22 15:51:04 -0600

    Clean up tests

--- test.js
+++ test.js
@@ -3,49 +3,20 @@ import isPresent from 'is-present'
 
 import coinr from './'
 
-test.cb('grabs ethereum data', t => {
-  t.plan(1)
-
-  coinr('ethereum')
-    .then(d => {
-      console.log(d)
-      t.true(isPresent(d))
-      t.end()
-    })
-    .catch(e => {
-      console.log(e)
-      t.true(false)
-      t.end()
-    })
+test('grabs ethereum data', async t => {
+  const res = await coinr('ethereum')
+  t.is(res.id, 'ethereum')
 })
 
-test.cb('grabs all currencies', t => {
-  t.plan(1)
-
-  coinr()
-    .then(d => {
-      console.log(d)
-      t.true(isPresent(d))
-      t.end()
-    })
-    .catch(e => {
-      console.log(e)
-      t.true(false)
-      t.end()
-    })
+test('grabs all currencies', async t => {
+  const res = await coinr()
+  t.true(isPresent(res))
 })
 
-test.cb('rejects with unknown currency', t => {
-  t.plan(1)
-
-  coinr('foobar')
-    .then(d => {
-      t.true(false)
-      t.end()
-    })
-    .catch(e => {
-      console.log(e)
-      t.true(true)
-      t.end()
-    })
+test('rejects with unknown currency', async t => {
+  try {
+    const res = await coinr('foobar')
+  } catch (e) {
+    t.pass()
+  }
 })
